8b815273a9855cdb2d4ca03196f04591d5928114
Pino Silvaggio <pino.silvaggio@gmail.com>
2013-05-17 03:06:02 -0400

    rename Resource to ResourceProvider

--- src/main/java/org/bitstrings/maven/plugins/splasher/ResourceProvider.java
+++ src/main/java/org/bitstrings/maven/plugins/splasher/ResourceProvider.java
@@ -4,8 +4,8 @@ import java.util.Map;
 
 import org.apache.maven.plugin.MojoExecutionException;
 
-public abstract class Resource
+public abstract class ResourceProvider
 {
-    public abstract Map<String, ?> resources( DrawingContext context )
+    public abstract Map<String, ?> resourceMap( DrawingContext context )
         throws MojoExecutionException;
 }
